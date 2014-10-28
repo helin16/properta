d8a128fb71e9cf3e762cc99f5736ae2658f12af6
l he <helin16@gmail.com>
2014-10-28 21:46:07 +1100

    imported COre

---
+++ core/main/conf/default.conf.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	'Database' => array(
+			'Driver' => 'mysql',
+			'DBHost' => 'localhost',
+			'DB' => 'bmv',
+			'Username' => 'root',
+			'Password' => 'root'
+		)
+	,'social' => array (
+		'addThisAccount' => 'helin16' 
+	)
+);
+
+?>
