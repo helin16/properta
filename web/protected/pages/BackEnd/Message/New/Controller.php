4c3cd6cd7eb3bde60ba176b46f8c95df83bb6049
Frank <tlan16@sina.com>
2014-12-28 22:13:26 +1100

    new message page captua bug

--- web/protected/pages/BackEnd/Message/New/Controller.php
+++ web/protected/pages/BackEnd/Message/New/Controller.php
@@ -15,12 +15,32 @@ class Controller extends FrontEndPageAbstract
     protected function _getEndJs()
     {
         $js = parent::_getEndJs();
-        $js .="pageJs.init('#main-form');";
+        $js .="pageJs.setCallbackId('contactus', '" . $this->contactUsBtn->getUniqueId() . "')";
+        $js .=".init('#main-form');";
         return $js; 
     }
-    public function getCaptchaKey()
-    {
-    	$googleRecap = Config::get('google', 'reCaptcha');
-    	return $googleRecap['public-key'] ;
-    }
+	public function getCaptchaKey()
+	{
+		$googleRecap = Config::get('google', 'reCaptcha');
+		return $googleRecap['public-key'] ;
+	}
+	public function contactUs($sender, $param)
+	{
+		$results = $errors = array();
+		try
+		{
+			$data = json_decode(json_encode($param->CallbackParameter), true);
+			$conf = Config::get('google', 'reCaptcha');
+			$resp = ReCaptcha::verifyResponse($conf['verify-url'], $_SERVER['REMOTE_ADDR'], $data['g-captcha'], $conf['secret-key']);
+			if ($resp === null || !$resp->success)
+				throw new Exception('Invalid Captcha Provided!');
+			EmailSenderAbstract::sendEmail(Config::get('contact-us', 'tos'), $this->getAppName() . ' Contact Us: ' . $data['subject'], $data['comments'], $data['comments'], $data['email'], $data['name']);
+		}
+		catch(Exception $ex)
+		{
+			$errors[] = $ex->getMessage();
+		}
+		$param->ResponseData = StringUtilsAbstract::getJson($results, $errors);
+		return $this;
+	}
 }
