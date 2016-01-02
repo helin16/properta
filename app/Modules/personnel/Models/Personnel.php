68380085df1c746d509bd7920de73e35b666214a
Kevin <wh00527@gmail.com>
2016-01-02 18:20:07 +1100

    2016-01-02 [Dev] user module finish for Dev version 6

--- app/Modules/personnel/Models/Personnel.php
+++ app/Modules/personnel/Models/Personnel.php
@@ -4,11 +4,25 @@ use Illuminate\Database\Eloquent\Model;
 use Session;
 use DB;
 use App;
+use App\Modules\User\Models\Role;
+use App\Modules\User\Models\User;
 
 class Personnel extends Model {
 
+    protected $table = 'admin_accesses';
+
 	protected function checkUserAccess(){
-        $currentUserID = Session::get('currentUser');
+        $currentUserId = Session::get('currentUser');
+        $currentUserRole = User::getCurrentUser($currentUserId);
+        $currentRole = Role::getCurrentRole($currentUserRole->role_id)->name;
+
+
+        if($currentUserRole->role_id == 1 || $currentUserRole->role_id == 2){
+            return true;
+        }
+
+
+
         return App::abort(403, 'Access denied');
 
         //return $currentUserID;
