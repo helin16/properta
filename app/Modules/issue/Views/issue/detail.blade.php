ed350ac0d3e5835400291e216653e1f30d2628d6
Frank Lan <franklan118@gmail.com>
2016-01-11 14:01:37 +1100

    reform issue pages (adapt template)

--- app/Modules/issue/Views/issue/detail.blade.php
+++ app/Modules/issue/Views/issue/detail.blade.php
@@ -1,9 +1,10 @@
-{{--{{ die(var_dump($rentals)) }}--}}
-
 @extends('rental::base.base')
 @section('container')
     {!! Form::open(['url' => '/issue', 'files' => true]) !!}
         @include('issue::issue.form', ['issue' => $issue])
-        {!! Form::button('Save', array('type' => 'submit', 'class' => 'btn btn-success')) !!}
+        <div class="hr-line-dashed"></div>
+        <div class="form-group">
+            {!! Form::button('Save', array('type' => 'submit', 'class' => 'btn btn-success')) !!}
+        </div>
     {!! Form::close() !!}
 @endsection
--- app/Modules/issue/Views/issue/list.blade.php
+++ app/Modules/issue/Views/issue/list.blade.php
@@ -1,36 +1,47 @@
 <?php setlocale(LC_MONETARY, 'en_AU.UTF-8') ?>
-@extends('rental::base.list')
-@section('item-list')
-    <div class="row">
-        <strong style="line-height: 30px;">Found {{ $data->total() }} Issues</strong>
-        {!! Form::open(['method' => 'GET', 'route' => 'issue.show', 'class' => 'pull-right', 'style'=>'display:inline-block']) !!}
-            {!! Form::button('Create', array('type' => 'submit', 'class' => 'btn btn-primary')) !!}
-        {!! Form::close() !!}
+@extends('abstracts::base.2_columns_left_nav_bar')
+@section('page_body')
+    <div class="wrapper wrapper-content animated fadeIn">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    {!! Form::open(['method' => 'GET', 'route' => 'issue.show', 'class' => 'pull-right', 'style'=>'display:inline-block']) !!}
+                        {!! Form::button('Create', array('type' => 'submit', 'class' => 'btn btn-primary')) !!}
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        @foreach($data->all() as $issue)
+            <div class="col-lg-12" property_id="{{ $property->id }}">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-content">
+                        <div class="row">
+                            <div class="col-sm-11">
+                                @include('rental::base.list_row', ['title' => ['content' => ucfirst('requester')], 'body' => ['content' => $issue->requester_user->inline() ]])
+                                @include('rental::base.list_row', ['title' => ['content' => ucfirst('status')], 'body' => ['content' => $issue->status ]])
+                                @include('rental::base.list_row', ['title' => ['content' => ucfirst('address')], 'body' => ['content' => '<a href=' . URL::route('property.show', ['property_id' => $issue->rental->property->id]) . '>' . $issue->rental->property->address->inline() . '</a>' ]])
+                                @include('rental::base.list_row', ['title' => ['content' => ucfirst('rental')], 'body' => ['content' => '<a href=' . URL::route('rental.show', ['property_id' => $issue->rental->property->id]) . '>' . money_format('%.2n', $issue->rental->property->rental()['averageDailyAmount']) . '</a>' ]])
+                                @foreach($issue->details->all() as $detail)
+                                    <hr/>
+                                    @include('rental::base.list_row', ['title' => ['content' => ucfirst('type')], 'body' => ['content' => $detail->type]])
+                                    @include('rental::base.list_row', ['title' => ['content' => ucfirst('priority')], 'body' => ['content' => $detail->priority]])
+                                    @include('rental::base.list_row', ['title' => ['content' => ucfirst('3rd Party')], 'body' => ['content' => $detail['3rdParty']]])
+                                    @include('rental::base.list_row', ['title' => ['content' => ucfirst('content')], 'body' => ['content' => $detail->content]])
+                                    @include('message::media.list', ['media' => $detail->media()->get()->all()])
+                                @endforeach
+                            </div>
+                            <div class="col-sm-1">
+                                {!! Form::open(['method' => 'GET', 'url' => URL::route('issue.show', $issue->id), 'style'=>'display:inline-block']) !!}
+                                    {!! Form::button('Update', array('type' => 'submit', 'class' => 'btn btn-primary')) !!}
+                                {!! Form::close() !!}
+                                {!! Form::open(['method' => 'DELETE', 'url' => URL::route('issue.destroy', $issue->id), 'style'=>'display:inline-block']) !!}
+                                    {!! Form::button('Delete', array('type' => 'submit', 'class' => 'btn btn-warning')) !!}
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        </div>
     </div>
-    @foreach($data->all() as $issue)
-        <li class="list-group-item row" issue_id="{{ $issue->id }}">
-            <div class="col-sm-10">
-                @include('rental::base.list_row', ['title' => ['content' => ucfirst('requester')], 'body' => ['content' => $issue->requester_user->inline() ]])
-                @include('rental::base.list_row', ['title' => ['content' => ucfirst('status')], 'body' => ['content' => $issue->status ]])
-                @include('rental::base.list_row', ['title' => ['content' => ucfirst('address')], 'body' => ['content' => '<a href=' . URL::route('property.show', ['property_id' => $issue->rental->property->id]) . '>' . $issue->rental->property->address->inline() . '</a>' ]])
-                @include('rental::base.list_row', ['title' => ['content' => ucfirst('rental')], 'body' => ['content' => '<a href=' . URL::route('rental.show', ['property_id' => $issue->rental->property->id]) . '>' . money_format('%.2n', $issue->rental->property->rental()['averageDailyAmount']) . '</a>' ]])
-                @foreach($issue->details->all() as $detail)
-                    <hr/>
-                    @include('rental::base.list_row', ['title' => ['content' => ucfirst('type')], 'body' => ['content' => $detail->type]])
-                    @include('rental::base.list_row', ['title' => ['content' => ucfirst('priority')], 'body' => ['content' => $detail->priority]])
-                    @include('rental::base.list_row', ['title' => ['content' => ucfirst('3rd Party')], 'body' => ['content' => $detail['3rdParty']]])
-                    @include('rental::base.list_row', ['title' => ['content' => ucfirst('content')], 'body' => ['content' => $detail->content]])
-                    @include('message::media.list', ['media' => $detail->media()->get()->all()])
-                @endforeach
-            </div>
-            <div class="col-sm-2">
-                {!! Form::open(['method' => 'GET', 'url' => URL::route('issue.show', $issue->id), 'style'=>'display:inline-block']) !!}
-                    {!! Form::button('Update', array('type' => 'submit', 'class' => 'btn btn-primary')) !!}
-                {!! Form::close() !!}
-                {!! Form::open(['method' => 'DELETE', 'url' => URL::route('issue.destroy', $issue->id), 'style'=>'display:inline-block']) !!}
-                    {!! Form::button('Delete', array('type' => 'submit', 'class' => 'btn btn-warning')) !!}
-                {!! Form::close() !!}
-            </div>
-        </li>
-    @endforeach
 @endsection
