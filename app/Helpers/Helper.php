b3bfa526ed740d31d2bcbedde05b5453dfb390ce
Kevin <wh00527@gmail.com>
2016-01-20 15:33:28 +1100

    2016-01-20 [Dev] left side bar logic change

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -3,11 +3,16 @@
 
 namespace App\Helpers;
 
-class Helper
-{
-    static function bob()
-    {
-        return '2';
+use Session;
+use App\Modules\Message\Models\Message;
+
+class Helper{
+
+    static function getAllMessages(){
+
+        $value = Session::get('currentUserId');
+        $model = Message::where('to_user_id',$value);
+        return $model;
     }
 }
 
